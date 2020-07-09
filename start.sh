04371ce90934aa59a9729b7694be74e6cd01437a
Adam Selyem <selyem.adam@gmail.com>
2020-07-09 21:59:51 +0200

    Remove --rm, resolve conflict with --restart

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 mkdir ./out
-sudo docker run -d --rm --restart=unless-stopped --name ssh_watch -v $PWD/out:/out ssh_watch
+sudo docker run -d --restart=unless-stopped --name ssh_watch -v $PWD/out:/out ssh_watch
